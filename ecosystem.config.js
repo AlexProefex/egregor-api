405df3c9b55293600912966d49632ad4ce8d4441
AlexProefex <102544315+AlexProefex@users.noreply.github.com>
2024-07-09 11:32:13 -0500

    update
    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,6 @@ module.exports = {
         exec_mode: 'cluster',
         instances: 'max',
         script: 'node -r dotenv/config ./dist/main.js',
-      },
-
-      {
-        name: 'adminDocs',
-        port: '3003',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: 'npx @compodoc/compodoc -p tsconfig.json -s -r 3003',
-      },
-
+      }
     ]
 }
