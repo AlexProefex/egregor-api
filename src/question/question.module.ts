5c20a820450e041edbdcd404fe576b0651369374
AlexProefex <102544315+AlexProefex@users.noreply.github.com>
2024-08-08 12:47:10 -0500

    update
    update

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -4,9 +4,10 @@ import { QuestionController } from './question.controller';
 import { QuestionEntity } from 'src/database/entity/question/question-entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StorageService } from 'src/storage/storage.service';
+import { QsectionEntity } from 'src/database/entity/qsection/qsection-entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([QuestionEntity])],
+  imports:[TypeOrmModule.forFeature([QuestionEntity, QsectionEntity])],
   controllers: [QuestionController],
   providers: [QuestionService,StorageService],
 })
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { notEqual } from 'assert';
+import { QsectionEntity } from 'src/database/entity/qsection/qsection-entity';
 import { QuestionEntity } from 'src/database/entity/question/question-entity';
 import { ExceptionErrorMessage } from 'src/validation/exception-error';
 import { Between, DataSource, LessThanOrEqual, MoreThan, MoreThanOrEqual, Not, Repository } from 'typeorm';
@@ -10,6 +11,7 @@ import { Between, DataSource, LessThanOrEqual, MoreThan, MoreThanOrEqual, Not, R
 export class QuestionService {
   constructor(@InjectRepository(QuestionEntity)
   private readonly questionRp: Repository<QuestionEntity>,
+  @InjectRepository(QsectionEntity) private readonly qsectionRp: Repository<QsectionEntity>,
   private datasource:DataSource) {
   }
 
@@ -26,11 +28,14 @@ export class QuestionService {
 
 
   //Guardar Question
-  async saveQuestion(question: any) {
+  async saveQuestion(questionData: any) {
     try {
-      const position = await this.questionRp.count({ where: { question: question.question } })
-      question.order = position + 1
-      return await this.questionRp.save(question);
+      const position = await this.questionRp.count({ where: { question: {id:questionData.question}} })
+      console.log(position,"gfdgfd", questionData.question)
+      questionData.order = position + 1
+      const test= await this.questionRp.save(questionData);
+      console.log(test)
+      return test
     } catch (error) {
       console.log(error)
       ExceptionErrorMessage(error);
@@ -43,19 +48,22 @@ export class QuestionService {
     const queryRunner = this.datasource.createQueryRunner()
     await queryRunner.startTransaction()
     try {
-
-        const current = await queryRunner.manager.withRepository(this.questionRp).findOneBy({ id: question.id })
+      console.log(question)
+      const current = await queryRunner.manager.withRepository(this.questionRp).findOne({where:{id: question.question}, relations:{question:true}})
+      console.log(current)
 
         if (current.order < question.newOrder) {
           //-1
-          const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: Between(current.order + 1, question.newOrder), question: question.question } })
+          const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: Between(current.order + 1, question.newOrder), question:{id:current.question.id} } })
+          console.log(propertiesElements)
           propertiesElements.map(async (question) => {
             await queryRunner.manager.withRepository(this.questionRp).update({ id: question.id }, { order: question.order - 1 })
           })
           await queryRunner.manager.withRepository(this.questionRp).update({ id: current.id }, { order: question.newOrder })
         }
         else if (current.order > question.newOrder) {
-          const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: Between(question.newOrder, current.order - 1), question: question.question } })
+          const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: Between(question.newOrder, current.order - 1), question:{id:current.question.id} }})
+          console.log(propertiesElements)
           propertiesElements.map(async (question) => {
             await queryRunner.manager.withRepository(this.questionRp).update({ id: question.id }, { order: question.order + 1 })
           })
@@ -65,6 +73,7 @@ export class QuestionService {
         await queryRunner.commitTransaction()
     
     } catch (err) {
+      console.log(err)
         await queryRunner.rollbackTransaction()
         ExceptionErrorMessage(err);
     } finally {
@@ -104,7 +113,8 @@ export class QuestionService {
 
   //Borrar Question
   async deleteQuestion(id: any) {
-    const exist = await this.questionRp.findOneBy({id:id});
+    const exist = await this.questionRp.findOne({where:{id: id}, relations:{question:true}})
+    
     if(!exist)
       return exist
     const queryRunner = this.datasource.createQueryRunner()
@@ -112,7 +122,7 @@ export class QuestionService {
     try {
       const current = await queryRunner.manager.withRepository(this.questionRp).findOneBy({ id: id })
 
-      const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: MoreThan(current.order) } })
+      const propertiesElements = await queryRunner.manager.withRepository(this.questionRp).find({ where: { order: MoreThan(current.order), question:{ id:exist.question.id}  } })
       propertiesElements.map(async (question) => {
         await queryRunner.manager.withRepository(this.questionRp).update({ id: question.id }, { order: question.order - 1 })
       })
